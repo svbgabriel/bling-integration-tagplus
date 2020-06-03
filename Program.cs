110c8ec4d0c2de4989de26533e150502eb47020c
Gabriel Batista <svbgabriel@gmail.com>
2020-06-02 22:51:40 -0300

    Implementa função para abrir o browser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace bling_integration_tagplus
 {
@@ -8,10 +10,28 @@ namespace bling_integration_tagplus
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Iniciando a integração...");
-            Console.WriteLine("Será necessário fazer o login no Tagplus");
-            Console.WriteLine("Por gentiliza, copie e cole o abaixo endereço no navegador");
-            Console.WriteLine($"https://developers.tagplus.com.br/authorize?response_type=token&client_id={CLIENT_ID}&scope=write:produtos+read:pedidos");
+            Console.WriteLine("Bem vindo a integração Bling - Tagplus");
+            Console.WriteLine();
+            Console.WriteLine("Será necessário autorizar a integração no Tagplus");
+            Console.WriteLine("O navegador será aberto para isso");
+            Console.WriteLine("Por gentiliza, siga as instruções e insira o código gerado");
+            OpenBrowser($"https://developers.tagplus.com.br/authorize?response_type=token&client_id={CLIENT_ID}&scope=write:produtos+read:pedidos");
+        }
+
+        static void OpenBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
         }
     }
 }
