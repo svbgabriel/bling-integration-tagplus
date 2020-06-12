80c8539bc3adb586c89cb540c1222a92ece289af
Gabriel Batista <svbgabriel@gmail.com>
2020-06-12 00:11:41 -0300

    Cria a classe OSUtils

--- Program.cs
+++ Program.cs
@@ -1,56 +1,49 @@
+using BlingIntegrationTagplus.Utils;
 using dotenv.net;
 using dotenv.net.Utilities;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace BlingIntegrationTagplus
 {
     class Program
     {
-        static string CLIENT_ID = "r7Qww3H1q0aZxpHqQFKAwa47gYdWxHjW";
+        static readonly string CLIENT_ID = "r7Qww3H1q0aZxpHqQFKAwa47gYdWxHjW";
 
         static void Main(string[] args)
         {
+            Console.WriteLine("############################################");
+            Console.WriteLine("## Bem vindo a integração Bling - Tagplus ##");
+            Console.WriteLine("############################################");
+            Console.WriteLine();
+
             // Carrega o arquivo de configuração
             DotEnv.Config();
             var envReader = new EnvReader();
 
+            // Inicializa o banco de dados local
             using (var db = new IntegrationContext())
             {
+                // Realiza as migrações
                 db.Database.Migrate();
+                // Verifica se o Token do Tagplus já está no banco de dados
                 var token = db.SettingStrings.SingleOrDefault(setting => setting.name.Equals("TagplusToken"));
                 if (token == null)
                 {
                     Console.WriteLine("O Token do Tagplus não foi encontrado no banco de dados");
+                    Console.WriteLine("Será necessário autorizar a integração no Tagplus");
+                    Console.WriteLine("O navegador será aberto para isso");
+                    Console.WriteLine("Por gentiliza, siga as instruções e insira o código gerado");
+                    OSUtils.OpenBrowser($"https://developers.tagplus.com.br/authorize?response_type=token&client_id={CLIENT_ID}&scope=write:produtos+read:pedidos");
+                    string code = Console.ReadLine();
+                    while (code.Length == 0)
+                    {
+                        Console.WriteLine("Não foi informado o código. Por gentiliza, informe o código:");
+                        code = Console.ReadLine();
+                    }
                 }
-            }
-
-            Console.WriteLine("Bem vindo a integração Bling - Tagplus");
-            Console.WriteLine();
-            Console.WriteLine("Será necessário autorizar a integração no Tagplus");
-            Console.WriteLine("O navegador será aberto para isso");
-            Console.WriteLine("Por gentiliza, siga as instruções e insira o código gerado");
-            OpenBrowser($"https://developers.tagplus.com.br/authorize?response_type=token&client_id={CLIENT_ID}&scope=write:produtos+read:pedidos");
-            string code = Console.ReadLine();
-        }
-
-        static void OpenBrowser(string url)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Process.Start("xdg-open", url);
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start("open", url);
-            }
+            }            
         }
     }
 }
---
+++ Utils/OSUtils.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BlingIntegrationTagplus.Utils
+{
+    class OSUtils
+    {
+        public static void OpenBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+        }
+    }
+}
