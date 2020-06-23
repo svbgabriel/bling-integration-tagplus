7e6b1b4b790d539ed1c8f8332b42870d13b54077
Gabriel Batista <svbgabriel@gmail.com>
2020-06-23 00:27:52 -0300

    Adiciona a classe de erro para o TagPlus

--- Clients/TagPlus/Models/TagPlusResponseError.cs
+++ Clients/TagPlus/Models/TagPlusResponseError.cs
@@ -1,7 +1,28 @@
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace BlingIntegrationTagplus.Clients.TagPlus.Models
 {
+    public class Error
+    {
+
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("dataPath")]
+        public string DataPath { get; set; }
+
+        [JsonProperty("schemaKey")]
+        public string SchemaKey { get; set; }
+
+        [JsonProperty("subResults")]
+        public object SubResults { get; set; }
+    }
+
+
     public class TagPlusResponseError
     {
         [JsonProperty("error_code")]
@@ -15,5 +36,8 @@ namespace BlingIntegrationTagplus.Clients.TagPlus.Models
 
         [JsonProperty("data")]
         public object Data { get; set; }
+
+        [JsonProperty("errors")]
+        public IList<Error> Errors { get; set; }
     }
 }
