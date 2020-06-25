e77a1b41539b0061af0a47a1340751f2b1e2b3a7
Gabriel Batista <svbgabriel@gmail.com>
2020-06-25 00:01:56 -0300

    Separa a recuperação do Token do Tagplus

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using BlingIntegrationTagplus.Clients.TagPlus.Models.Clientes;
 using BlingIntegrationTagplus.Clients.TagPlus.Models.Pedidos;
 using BlingIntegrationTagplus.Databases;
 using BlingIntegrationTagplus.Exceptions;
-using BlingIntegrationTagplus.Models;
 using BlingIntegrationTagplus.Utils;
 using dotenv.net;
 using dotenv.net.Utilities;
@@ -13,14 +12,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 
 namespace BlingIntegrationTagplus
 {
     class Program
     {
-        static readonly string CLIENT_ID = "r7Qww3H1q0aZxpHqQFKAwa47gYdWxHjW";
-        static readonly string SCOPE = "read:produtos+read:formas_pagamento+read:departamentos+read:usuarios+write:pedidos+write:clientes";
 
         static void Main(string[] args)
         {
@@ -39,44 +35,9 @@ namespace BlingIntegrationTagplus
             // Realiza as migrações
             db.Database.Migrate();
             // Verifica se o Token do Tagplus já está no banco de dados e válido
-            string code;
-            var token = db.TagPlusTokens.SingleOrDefault(setting => setting.Name.Equals("TagplusToken"));
-            if (token == null || DateTime.Now.CompareTo(DateTime.Parse(token.ExpiresIn)) >= 0)
-            {
-                Console.WriteLine();
-                Console.WriteLine("O Token do Tagplus não foi encontrado no banco de dados ou está expirado");
-                Console.WriteLine("Será necessário autorizar a integração no Tagplus");
-                Console.WriteLine("O navegador será aberto para isso");
-                Console.WriteLine("Por gentiliza, siga as instruções e insira o código gerado");
-                OSUtils.OpenBrowser($"https://developers.tagplus.com.br/authorize?response_type=token&client_id={CLIENT_ID}&scope={SCOPE}");
-                code = Console.ReadLine();
-                while (string.IsNullOrWhiteSpace(code))
-                {
-                    Console.WriteLine("Não foi informado o código. Por gentiliza, informe o código:");
-                    code = Console.ReadLine();
-                }
-                // Gera a data de expiração
-                // O Token expira em 24 horas, mas como "folga" ele será renovado em 20 horas
-                DateTime expirationDate = DateTime.Now.AddHours(20);
-                // Remove o registro antigo
-                if (token == null)
-                {
-                    db.Add(new TagPlusToken("TagplusToken", code, expirationDate.ToString("dd/MM/yyyy HH:mm:ss")));
-                }
-                else
-                {
-                    token.Value = code;
-                    token.ExpiresIn = expirationDate.ToString("dd/MM/yyyy HH:mm:ss");
-                    db.Update(token);
-                }
-                db.SaveChanges();
-            }
-            else
-            {
-                code = token.Value;
-            }
-            Console.WriteLine();
+            string code = DBUtils.RetriveTagPlusToken(db);
             // Recupera os pedidos do Bling
+            Console.WriteLine();
             Console.WriteLine("Procurando pedidos no Bling...");
             var blingClient = new BlingClient(blingApiKey);
             PedidosResponse pedidos = null;
