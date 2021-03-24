cf84c0a738697212ea9fcfda24112d41a8e4c4da
Gabriel Batista <svbgabriel@gmail.com>
2021-03-24 20:03:21 -0300

    Remove "$" desnecessário

--- Services/ProdutoService.cs
+++ Services/ProdutoService.cs
@@ -129,7 +129,7 @@ namespace BlingIntegrationTagplus.Services
                 {
                     Itens = itens,
                     Fornecedor = idFornecedor,
-                    Observacoes = $"Número do Pedido Tagplus: {numero}\nEntrega até: ${DateTime.Now.AddDays(7)}"
+                    Observacoes = $"Número do Pedido Tagplus: {numero}\nEntrega até: {DateTime.Now.AddDays(7)}"
                 };
 
                 itensCompra.Add(pedidoCompraBody);
